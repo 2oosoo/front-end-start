df890ee42699e177ca2be6a414d260b77e2ba0a5
parkhyunkyu <parkhyun52@gmail.com>
2018-09-07 23:47:39 +0900

    second commit fixed

---
+++ 02/app.js
@@ -0,0 +1,21 @@
+for(i=1; i<=100; i++) {
+    console.log(i);
+}
+
+for(i=1; i<=100; i++) {
+    if(i%2==0) {
+        console.log(i);
+    }
+}
+
+for(i=1; i<=100; i++) {
+    if(i%2==1) {
+        console.log(i);
+    }
+}
+
+for(i=2; i<=9; i++) {
+    for(j=1; j<=9; j++) {
+        console.log(i*j);
+    }
+}
