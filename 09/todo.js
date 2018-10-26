897f8227f93a16c0a6020827240e41f6292e1121
parkhyunkyu <parkhyun52@gmail.com>
2018-10-26 18:02:14 +0900

    delete button not done

---
+++ 09/todo.js
@@ -0,0 +1,46 @@
+//start
+
+//input 입력 받기
+
+var input = document.getElementById('memo');
+var form = document.querySelector('.new-task');
+var list = document.getElementById('list'); //document.querySeletor('#list')
+
+// debugger; // 코드를 멈추게 함
+
+function addTodo(event) {
+    var todo = input.value;
+    console.log(todo);
+    insertTodo(todo);
+    input.value = '';
+
+    event.preventDefault();
+}
+
+function insertTodo(todo) {
+    var html = `<li>
+    <button class="delete">×</button>
+    <input type="checkbox" class="toggle-checked">
+    <span class="text">${todo}</span>
+    </li>`;
+
+    list.innerHTML += html;
+}
+
+
+// function addTodo(event) {   //
+//     if(event.keyCode === 13) {
+//         var todo = input.value;
+//         console.log(event);
+//     }
+// }
+
+// input.addEventListener('keydown', addTodo);
+
+form.addEventListener('submit', addTodo);
+
+// btn 클릭 이벤트 만들어 놓기
+function deleteTodo(list) {
+    var child = list.child;
+    var btn = list.getElementByClassName('')
+}
